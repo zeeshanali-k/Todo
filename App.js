5b345acc1df986e0bb6bb8f948324ada7f39d7d4
Zeeshan Ali <zeeshanalik73@gmail.com>
2021-01-19 17:29:51 +0500

    Trying

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, View ,Text, Alert , FlatList } from 'react-native';
+import { StyleSheet, View ,Text, Alert ,Image, FlatList } from 'react-native';
 import Adder from './components/Adder';
 var count=0;
 
@@ -16,22 +16,33 @@ function clickListener(txt){
     )
   }else{
     Alert.alert("Please enter something to proceed!")
-  }
+    }
 }
 
   return (
+    
     <View style={styles.container_col}>
-      <StatusBar style="auto" />
-         <Adder style={{flex:1}} onAdded={clickListener}/>
-       <FlatList
+      <StatusBar style="inverted" backgroundColor="#bbb"/>
+      
+      <Adder style={{flex:1}} onAdded={clickListener}/>
+      
+      <FlatList
           style={{flex:3}}
           data={todos}
           renderItem={({item})=>(
+            <View style={styles.list_style}>
+              <Image
+              style={styles.tinyLogo}
+              source={{uri:"../circle.png"}}
+              />
               <Text style={styles.itemStyle}>{item.text}</Text>
+            </View>
           )}
+
         />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -39,10 +50,30 @@ const styles = StyleSheet.create({
     backgroundColor:"#fff",
     alignItems: 'center',
     flex:1,
+    flexDirection:"column",
+    borderColor:"#bbb",
+    borderWidth:1,
+    marginTop:25,
+    marginStart:5,
+    marginEnd:5,
+    marginBottom:5,
   },
+
+  list_style:{
+    flexDirection:'row',
+    flex:1,
+  },
+
   itemStyle:{
     padding:5,
     backgroundColor:"#ff21ff",
     margin:5,
+    // flex:3,
+  },
+  
+  tinyLogo:{
+    width:50,
+    height:50,
+    flex:1,
   }
 });
